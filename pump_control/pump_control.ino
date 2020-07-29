e16902dd6bb610c70e1815abf6b93327d113510b
Rodrigo Ledesma-Pinet <rodrigopinet@outlook.com>
2020-07-29 11:36:55 -0500

    Added wait time to get into the cabin

--- pump_control/pump_control.ino
+++ pump_control/pump_control.ino
@@ -1,11 +1,13 @@
 int greenLED = 3;
-int redLED = 1;
+int redLED = 4;
 int pir1 = 8;
 int pir2 = 10;
 int initTime = 60;
 int sanitTime = 6;
 int exitTime = 2;
 int relayPin = 12;
+int counter = 0;
+int pirState = LOW;
 void setup()
 {
   //Pin mode for LEDS
@@ -16,20 +18,30 @@ void setup()
   pinMode(pir2, INPUT);
   //Pin mode for relay
   pinMode(relayPin, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop()
 {
   pirInitialization();
   //Turn on green LED to signal ready
+  while(1){
   digitalWrite(greenLED, HIGH);
+  digitalWrite(redLED, LOW);
    //Wait for sensor reading. 
 
     if(digitalRead(pir1) == HIGH || digitalRead(pir2) == HIGH){
-    //Motion detected
-    Serial.println("Motion detected");
-    startSanitization();
-  }
+      if(pirState == LOW){
+        Serial.print("Motion detected. Count:  ");
+        Serial.println(counter);
+        counter++;
+        startSanitization();
+        pirState = HIGH;
+      }
+      else{
+        pirState = LOW;
+      }
+  }}
     
 }
 
@@ -47,7 +59,8 @@ void pirInitialization(){
     digitalWrite(redLED, LOW);
     //Wait for 1/2 second
     delay(500);
-    Serial.println("Initializing... " + i);
+    Serial.print("Initializing... ");
+    Serial.println(i);
   }
 }
 
@@ -57,17 +70,15 @@ void startSanitization(){
   //Turn on red light
   digitalWrite(redLED, HIGH);
   //Turn on pump
+  //Wait for person to get in.
+  delay(2000);
+  Serial.println("Turning on pump...");
   digitalWrite(relayPin, HIGH);
   delay(sanitTime*1000-100);
   digitalWrite(relayPin, LOW);
-  //Wait for sanitization to complete
-  
-  //Turn off pump
-  digitalWrite(relayPin, HIGH);
+  Serial.println("Turning off pump..."); 
   delay(exitTime*1000-100);
   digitalWrite(relayPin, LOW);
-  //Wait for person to get out
-  
   //Turn off red light
   digitalWrite(redLED, LOW);
   //Turn on green light
